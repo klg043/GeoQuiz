33534f47b37be33d16dd89195a083cda719bf46d
Kai <kgannon@iszolea.net>
2022-08-29 23:25:23 +1000

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.geoquiz
+
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+}
